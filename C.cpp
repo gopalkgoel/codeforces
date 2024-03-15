0bc07c9818da85ff70379b27e18eccdba417e2c5
Gopal Goel <gopal.krishna.goel@gmail.com>
2024-03-15 11:30:24 -0400

    .

--- C.cpp
+++ C.cpp
@@ -3,34 +3,37 @@ using namespace std;
 
 typedef long long ll;
 
+int solve(string row1, string row2, int n) {
+    int reachable[n];
+    reachable[0] = 1;
+    reachable[1] = (row2[0]=='>');
+    for(int i=2;i<n;i++) {
+         if(i%2 == 0) reachable[i] = (row1[i-1]=='>') && (reachable[i-1] || reachable[i-2]);
+         else reachable[i] = (row2[i-1]=='>') && (reachable[i-1] || reachable[i-2]);
+    }
+    return reachable[n-1];
+}
+
 int main ()
 {
-    std::ios_base::sync_with_stdio(false), cin.tie(0) , cout.tie(0);
-    freopen("input.txt", "r", stdin);
+    // std::ios_base::sync_with_stdio(false), cin.tie(0) , cout.tie(0);
+    // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
     int t;
     cin >> t;
+
     int n;
-    //char ans[200];
-    char current = 'A';
+
+    string row1;
+    string row2;
 
     while(t--) {
         cin >> n;
-        if(n%2 == 1) {
-            cout << "NO" << endl;
-        } else {
-            current = 'A';
-            cout << "YES" << endl;
-            for(int i=0;i<n/2;i++) {
-                cout << current << current;
-                if(current=='A') current = 'B';
-                else current = 'A';
-            }
-            cout << endl;
-        }
+        cin >> row1;
+        cin >> row2;
+        cout << (solve(row1,row2,n) ? "YES\n" : "NO\n");
     }
-
 
     
     return 0;
